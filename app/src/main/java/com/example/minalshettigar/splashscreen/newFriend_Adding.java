87b7141c7ba94f13bf8dad4e9d45533bc80447e2
Viral Prajapati <prajapativiral33@gmail.com>
2018-06-09 13:50:00 -0700

    NewFriendsAdding eerror resolve

--- app/src/main/java/com/example/minalshettigar/splashscreen/newFriend_Adding.java
+++ app/src/main/java/com/example/minalshettigar/splashscreen/newFriend_Adding.java
@@ -258,8 +258,7 @@ public class newFriend_Adding extends AppCompatActivity
 
     }
 
-    @Override
-}
+
     /*@Override
     protected void onActivityResult ( int requestCode, int resultCode, Intent intent){
         switch (requestCode) {
